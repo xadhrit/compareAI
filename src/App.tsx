e14d9612781922a217240d1ece387ab021c16ad0
xadhrit <maharajrabel@gmail.com>
2023-10-13 19:28:49 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+/**
+ * CSS module 
+ */
+
+import './index.css';
+
+function App() {
+  return (
+    <div className="App">
+    <header className='App-header'>
+      <h1>CompareAI </h1>
+    </header>
+    </div>
+  );
+}
+
+export default App;
