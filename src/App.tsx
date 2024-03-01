19f23a891ae48f807ae34c0839e678c50900fbf1
xadhrit <maharajrabel@gmail.com>
2024-03-01 19:33:52 +0530

    no more code in it

--- src/App.tsx
+++ src/App.tsx
@@ -125,7 +125,7 @@ const App: React.FC = () => {
     e.preventDefault();
     getTable();
     setImages([]);
-    setImagePreviews([]);
+    //setImagePreviews([]);
   }
 
   const handleUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
